0a751ec94843812f2f22c7176a6aa090d9b4b30f
Nachiappan <nachiapan@gmail.com>
2020-04-16 21:01:03 +0530

    Part 1

--- build-requestor/index.js
+++ build-requestor/index.js
@@ -1,10 +1,27 @@
 var htmlInterface = require('./htmlInterface');
+const axios = require('axios');
 
 async function process(){
-    var token = htmlInterface.getToken();
+    await valiateToken();
+
+    console.log(res);
+    console.log("done");
     
 }
 
 console.log(htmlInterface.getBranch());
 
-process().then(x => console.log("Went well")).catch(x => console.log("There is an error"));
+process().then(x => console.log("Went well")).catch(x => console.log(x.message));
+
+async function valiateToken() {
+    var token = htmlInterface.getToken();
+    try {
+        let res = await axios.get("https://api.github.com/user");
+    }
+    catch (e) {
+        if (e.response.status == 401)
+            throw new Error("The token is incorrect");
+        throw new Error("There may be a problem with the token");
+    }
+}
+
