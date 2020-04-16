84ed21703a7f3f52093ed30fb78ccda8a0c88cfa
Nachiappan <nachiapan@gmail.com>
2020-04-16 21:22:56 +0530

    Done

--- build-requestor/index.js
+++ build-requestor/index.js
@@ -4,7 +4,10 @@ const axios = require('axios');
 async function process(){
     await valiateToken();
 
-    console.log(res);
+    let organizationName = 'ZeroWasteTeam';    
+    let repositoryNames = await getRepositoryNames(organizationName);
+    htmlInterface
+    console.log(repositoryNames);
     console.log("done");
     
 }
@@ -13,10 +16,38 @@ console.log(htmlInterface.getBranch());
 
 process().then(x => console.log("Went well")).catch(x => console.log(x.message));
 
+async function getRepositoryNames(organizationName) {
+    let repositoryNames;
+    try {
+        let config = {
+            headers: {
+                'Authorization': `token ${htmlInterface.getToken()}`,
+                'Accept': 'application/vnd.github+json',
+            }
+        };
+        let res = await axios.get(`https://api.github.com/orgs/${organizationName}/repos`, config);
+        repositoryNames = res.data.map(x => x.full_name);
+    }
+    catch (e) {
+        console.log(e);
+        if (e.response.status == 404)
+            throw new Error("The organization is not found");
+        throw new Error("There may be a problem with the organization");
+    }
+    return repositoryNames;
+}
+
 async function valiateToken() {
     var token = htmlInterface.getToken();
     try {
-        let res = await axios.get("https://api.github.com/user");
+
+        let config = {
+            headers: {
+                'Authorization': `token ${token}`,
+                'Accept' : 'application/vnd.github+json',
+            }
+          }
+        let res = await axios.get("https://api.github.com/user",  config);
     }
     catch (e) {
         if (e.response.status == 401)
@@ -24,4 +55,11 @@ async function valiateToken() {
         throw new Error("There may be a problem with the token");
     }
 }
+
+
+
+
+
+
+
 
