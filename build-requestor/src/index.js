e880200d4c5d1f97219a2afb279a5f0c1a7c69af
Nachiappan <nachiapan@gmail.com>
2020-04-17 14:10:44 +0530

    Completed to some extent

--- build-requestor/src/index.js
+++ build-requestor/src/index.js
@@ -3,12 +3,17 @@ const axios = require('axios');
 
 module.exports = {
     onPageLoad: function () {
+        console.log("Page loaded started");
         populateToken();
         populateOrganziation();
-        console.log("Tokena"+document.getElementById("form-token").value);
-        setRepositoryNames();
+        setRepositoryNames()
+            .then(x => console.log("Page Load Completd Successfully"))
+            .catch(x => {
+                htmlInterface.setError(x.message);
+                console.log(`Page load completed with errors : ${x.message}`);
+            });
     },
-    
+
     onTokenChange: function () {
         console.log("Token has been changed");
         persistToken();
@@ -19,14 +24,30 @@ module.exports = {
                 console.log(`Token change completed with errors: ${x.message}`);
             });
     },
-    
-    onOrganizationChange: function (){
-        console.log("Repository has been changed");
+
+    onOrganizationChange: function () {
+        console.log("Organization has been changed");
         persistOrganization();
-        setRepositoryNames().then(x => console.log("Repository Change completed successfully")).catch(x => htmlInterface.setError(x.message));
+        setRepositoryNames()
+            .then(x => console.log("Organziation Change completed successfully"))
+            .catch(x => {
+                console.log("Organziation Change completed with errors: " + x.message);
+                htmlInterface.setError(x.message);
+            });
     },
 
-    onInputChangeSyncInterface: function(){
+    onRepositoryChange: function () {
+        console.log("Repository has been changed");
+
+        setBranchNames()
+            .then(x => console.log("Repository Change completed successfully"))
+            .catch(x => {
+                console.log("Repository Change completed with errors: " + x.message);
+                htmlInterface.setError(x.message);
+            });
+    },
+
+    onInputChangeSyncInterface: function () {
         setRepositoryNames();
     }
 };
@@ -41,53 +62,49 @@ function persistToken() {
 
 function persistOrganization() {
     var organization = htmlInterface.getOrganization();
-    console.log("organization is "+organization);
+    console.log("organization is " + organization);
     let localStorageOrganization = localStorage.getItem("organization");
     if (organization == null) organization = "";
-    if (localStorageOrganization != organization){
+    if (localStorageOrganization != organization) {
         localStorage.setItem("organization", organization);
-        console.log("LocalStorage org"+localStorage.getItem("organization"));
+        console.log("LocalStorage org" + localStorage.getItem("organization"));
     }
 }
 
-
-
 function populateToken() {
-      let token = localStorage.getItem("token");
-      if(token != null){
-          document.getElementById('form-token').value = token;
-      }
-  }
+    let token = localStorage.getItem("token");
+    if (token != null) {
+        document.getElementById('form-token').value = token;
+    }
+}
 
-  function populateOrganziation() {
-      let organziation = localStorage.getItem("organization");
-      if(organziation != null){
-          document.getElementById('form-organization').value = organziation;
-      }
-  }
+function populateOrganziation() {
+    let organziation = localStorage.getItem("organization");
+    if (organziation != null) {
+        document.getElementById('form-organization').value = organziation;
+    }
+}
 
-
-  function onPageLoad() {
-      populateToken();
-      populateOrganziation();
-      console.log("Token"+document.getElementById("form-token").value);
-      onTokenChangeSync();
-  }
-
-async function setRepositoryNames(){
+async function setRepositoryNames() {
     assertBasicInputsAreNotEmpty();
     await valiateToken();
 
     let repositoryNames = await getRepositoryNames();
     htmlInterface.setRepositoryNames(repositoryNames);
-    
+
     /*s
-    let branches = await getBranchNames();
-    htmlInterface.setBranchNames(branches);
+    
 
     let commitIds = await getCommitIds();
     htmlInterface.setCommitIds(commitIds);
     */
+}
+
+async function setBranchNames() {
+    //Validation needed
+
+    let branches = await getBranchNames();
+    htmlInterface.setBranchNames(branches);
 }
 
 function assertBasicInputsAreNotEmpty() {
@@ -106,8 +123,7 @@ async function getCommitIds() {
     try {
         let res = await axios.get(`https://api.github.com/repos/${organizationName}/${repoName}/commits?sha=${branchName}`, getConfig());
         return res.data.map(x => x.sha);
-    }
-    catch (e) {
+    } catch (e) {
         if (e.response.status == 404)
             throw new Error("The repoName is not found");
         throw new Error("There may be a problem with the repoName");
@@ -117,12 +133,11 @@ async function getCommitIds() {
 async function getBranchNames() {
     let organizationName = htmlInterface.getOrganization();
     let repoName = htmlInterface.getRepository();
-    console.log("THe repo name is#"+repoName);
+    console.log("THe repo name is#" + repoName);
     try {
         let res = await axios.get(`https://api.github.com/repos/${organizationName}/${repoName}/branches`, getConfig());
         branches = res.data.map(x => x.name);
-    }
-    catch (e) {
+    } catch (e) {
         if (e.response.status == 404)
             throw new Error("The repoName is not found");
         throw new Error("There may be a problem with the repoName");
@@ -134,9 +149,8 @@ async function getRepositoryNames() {
     let organizationName = htmlInterface.getOrganization();
     try {
         let res = await axios.get(`https://api.github.com/orgs/${organizationName}/repos`, getConfig());
-        return res.data.map(x => x.full_name).map(x => x.replace(`${organizationName}/`,''));
-    }
-    catch (e) {
+        return res.data.map(x => x.full_name).map(x => x.replace(`${organizationName}/`, ''));
+    } catch (e) {
         if (e.response.status == 404)
             throw new Error("The organization is not found");
         throw new Error("There may be a problem with the organization");
@@ -155,19 +169,10 @@ function getConfig() {
 
 async function valiateToken() {
     try {
-        let res = await axios.get("https://api.github.com/user",  getConfig());
-    }
-    catch (e) {
+        let res = await axios.get("https://api.github.com/user", getConfig());
+    } catch (e) {
         if (e.response.status == 401)
             throw new Error("The token is incorrect");
         throw new Error("There may be a problem with the token");
     }
 }
-
-
-
-
-
-
-
-
